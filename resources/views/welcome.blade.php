5931a69d122c796891dd99194b1d03c5a39bfcf0
fachriansyahmni <fachriansyahmni@gmail.com>
2020-07-05 18:20:20 +0700

    quiz 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,17 +83,9 @@
                 <div class="title m-b-md">
                     Sanbercode
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <a href="/artikel">
+                 <img src="{{ asset('erd.png') }}" alt="">
+                </a>
             </div>
         </div>
     </body>
